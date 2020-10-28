a00af92bc20e23daf82a99b7e6d3a4be7b824260
jatasingh128 <45856187+jatasingh128@users.noreply.github.com>
2020-10-28 20:20:04 +0530

    commit
    commit to code

---
+++ src/app/calendar-settings.ts
@@ -0,0 +1,7 @@
+export class CalendarSettings {
+    bookingColor: string;
+    calendar: { [key: string]: Object } = { start: '', end: '' };
+    currentView: string;
+    interval: number;
+    firstDayOfWeek: number;
+}
